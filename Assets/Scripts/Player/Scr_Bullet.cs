724d8358c2b94295aff5fac19a46416a70c99583
VictorFernandez7 <victorfernandez95@hotmail.com>
2018-09-24 00:02:52 +0200

    Prototipo 1 Finalizado
    - Movimiento
    - Salto
    - Agachado
    - Disparo en las cuatro direcciones
    - Impulso con disparo hacia abajo

---
+++ Assets/Scripts/Player/Scr_Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    public Scr_PlayerShooting player;
+
+    private void Start()
+    {
+        if (player.gunLookingUp || player.gunLookingDown)
+            rb.velocity = transform.up * speed;
+        else
+            rb.velocity = transform.right * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player")
+            Destroy(gameObject);
+    }
+}
